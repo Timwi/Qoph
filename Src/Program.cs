6ae211cf0b574d0eab1e67f4e17fdeab90ce4986
Timwi <github@timwi.de>
2020-09-24 02:39:04 +0200

    Finish some more puzzles; add credits for the sound files; fix Generated Crossword.svg

--- Src/Program.cs
+++ Src/Program.cs
@@ -35,7 +35,7 @@ namespace Qoph
                 return PostBuildChecker.RunPostBuildChecks(args[1], Assembly.GetExecutingAssembly());
 
 
-            TheNuke.Generate();
+            FaceToFace.GenerateCrossword();
 
 
             Console.WriteLine("Done.");
