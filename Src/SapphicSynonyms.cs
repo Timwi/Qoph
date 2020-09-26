b279204568050d57886c5228175934f2abb5e69e
Timwi <github@timwi.de>
2020-09-26 15:27:29 +0200

    Sapphic Synonyms update: LINE=ROW and ASSASSIN=HIRED KILLER were not considered clear/unambiguous enough

---
+++ Src/SapphicSynonyms.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using Qoph.Modeling;
+using RT.Util.ExtensionMethods;
+
+namespace Qoph
+{
+    static class SapphicSynonyms
+    {
+        public static void Generate()
+        {
+            var data = @"BIBLE/1/44,BABIES/4/7,INSANE/4/5,SEA/4/5,BALL/5/6,ALLIES/6/7".Split(',').Select(row => row.Split('/')).Select(arr => (word: arr[0], index: int.Parse(arr[1]), length: arr[2])).ToArray();
+            General.ReplaceInFile(@"D:\c\Qoph\EnigmorionFiles\sapphic-synonyms.html",
+                "<!--%%-->", "<!--%%%-->",
+                data.Select(tup => $@"<div class='box'><div class='stripes'>{tup.word.Select(ch => $@"<div class='stripe c-{"LESBIAN".IndexOf(ch) + 1}'></div>").JoinString()}</div><div class='extra'><div class='stripe c-{tup.index}'></div> / {tup.length.Select(ch => $"<div class='stripe c-{ch}'></div>").JoinString(" ")}</div></div>").JoinString());
+        }
+    }
+}
