427624b09dce652e2ba45a821dd553792d257d94
Timwi <github@timwi.de>
2021-09-28 11:24:17 +0200

    Solution pages for Swerve and Comebacks

--- Src/Program.cs
+++ Src/Program.cs
@@ -35,7 +35,7 @@ namespace Qoph
                 return PostBuildChecker.RunPostBuildChecks(args[1], Assembly.GetExecutingAssembly());
 
 
-            //
+            
 
 
             Console.WriteLine("Done.");
