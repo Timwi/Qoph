56039f7971b970f10b1c594412bd0d0bb8a6ad67
Timwi <github@timwi.de>
2022-04-28 17:07:22 +0200

    Didn’t notice that one of the images in Something’s Fishy was oversized!

--- Src/Program.cs
+++ Src/Program.cs
@@ -35,7 +35,7 @@ namespace Qoph
                 return PostBuildChecker.RunPostBuildChecks(args[1], Assembly.GetExecutingAssembly());
 
 
-            SapphicSynonyms.Generate();
+            SomethingsFishy.Generate();
 
 
             Console.WriteLine("Done.");
